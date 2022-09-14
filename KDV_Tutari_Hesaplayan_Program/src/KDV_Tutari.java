9a03aafe66ca4269aef0c33b25cff09fdc4aff44
Berkant Karakayış <102322084+berkantkarakayis@users.noreply.github.com>
2022-09-14 18:17:36 +0300

    Add files via upload

---
+++ KDV_Tutari_Hesaplayan_Program/src/KDV_Tutari.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class KDV_Tutari {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+
+        System.out.print("Ürün Fiyatını Giriniz:");
+        int urun= input.nextInt();
+
+        boolean kosul= urun<=1000;
+
+        double kdv= kosul ? 0.18 : 0.08;
+        double kdvTutar=kdv*urun;
+        double kdvUrun=kdvTutar+urun;
+
+        System.out.println("Ürünün KDV'li Fiyatı : " +kdvUrun);
+        System.out.println("KDV Oranı : " + kdv);
+        System.out.print("KDV Tutarı : " + kdvTutar);
+
+    }
+}
